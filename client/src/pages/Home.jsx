ac4530455b2d86568664fad7217a9eb1703789dd
Avinash <avidevelop60@gmail.com>
2026-07-30 17:04:54 +0530

    photo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -70,7 +70,7 @@ const Home = () => {
           <img
             src={mobileBanners}
             alt="Dailyfix Banner"
-            className="w-full h-full -mt-20 object-contain object-center sm:hidden"
+            className="w-full h-full -mt-10 object-contain object-center sm:hidden"
           />
 
           <img
@@ -89,7 +89,7 @@ const Home = () => {
             className="space-y-8 text-left max-w-xl"
           >
             {/* Brand Badge */}
-            <div className='px-5 sm:px-20 mb-40 sm:mb-0'>
+            <div className='px-5 sm:px-20 mb-30 sm:mb-0'>
               <div className="inline-flex items-center gap-2 mb-5 bg-white/10 border border-white/20 px-4 py-1.5 rounded-full backdrop-blur-md">
                 <span className="w-2 h-2 rounded-full bg-emerald-500 animate-pulse"></span>
                 <p className="text-white font-bold text-xs tracking-widest uppercase">Dailyfix Grooming</p>
@@ -105,7 +105,7 @@ const Home = () => {
             <div className="flex flex-wrap gap-4 px-10 sm:px-20 ">
               <Link
                 to="/shop"
-                className="group -mb-20 mt-20 bg-emerald-500 text-white font-bold py-4 px-10 rounded-full hover:bg-emerald-400 transition-all duration-300 shadow-xl hover:shadow-2xl hover:-translate-y-0.5 inline-flex items-center gap-2"
+                className="group -mb-10 mt-30 bg-emerald-500 text-white font-bold py-4 px-10 rounded-full hover:bg-emerald-400 transition-all duration-300 shadow-xl hover:shadow-2xl hover:-translate-y-0.5 inline-flex items-center gap-2"
               >
                 Shop Now
                 <ArrowRight className="w-4 h-4 transform group-hover:translate-x-1 transition-transform" />
@@ -302,7 +302,7 @@ const Home = () => {
 
             <div className="my-6 lg:my-0 flex justify-center">
               <div className="w-full  lg:max-w-none bg-gradient-to-br from-gray-200 to-gray-300 rounded-3xl overflow-hidden shadow-2xl">
-                <img src={banners} alt="Dailyfix Product" className="w-full h-[450px] sm:h-[450px] lg:h-[500px] object-cover" />
+                <img src={banners} alt="Dailyfix Product" className="w-full h-[500px] sm:h-[500px] lg:h-[600px] object-cover" />
               </div>
             </div>
 
